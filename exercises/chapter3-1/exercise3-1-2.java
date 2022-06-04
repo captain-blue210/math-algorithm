03e2b0657decbc810bdd04cda83698f745db68dd
captain-blue210 <captain.blue210@gmail.com>
2022-06-04 12:23:24 +0900

    refactor: formatting

--- exercises/chapter3-1/exercise3-1-2.java
+++ exercises/chapter3-1/exercise3-1-2.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
  */
 class Main {
    public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in);
-
-      long N = scanner.nextLong();
-      for (long i = 2; i * i <= N; i++) {
-         while (N % i == 0) {
-            System.out.print(i + " ");
-            N /= i;
+      try (Scanner scanner = new Scanner(System.in)) {
+         long N = scanner.nextLong();
+         for (long i = 2; i * i <= N; i++) {
+            while (N % i == 0) {
+               System.out.print(i + " ");
+               N /= i;
+            }
          }
-      }
 
-      if (N >= 2) {
-         System.out.print(N);
+         if (N >= 2) {
+            System.out.print(N);
+         }
       }
    }
 }
